622e4f265f006b2de55b87d571915c315c5f70de
Andy Liu <aliu927@gmail.com>
2021-03-10 21:56:35 -0800

    completed task app in react

--- src/component/Overview.js
+++ src/component/Overview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import uniqid from "uniqid";
 
 const Overview = (props) => {
   const { tasks } = props;
@@ -6,7 +7,7 @@ const Overview = (props) => {
   return (
     <ul>
       {tasks.map((task) => {
-        return <li>{task}</li>;
+        return <li key={uniqid()}>{task}</li>;
       })}
     </ul>
   );
